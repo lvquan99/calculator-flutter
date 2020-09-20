a722fc5f6812993ba0febf8208f65c50f46568e3
lvquan99 <quanlv149@gmail.com>
2020-09-20 17:03:18 +0700

    simple

---
+++ android/app/src/main/kotlin/com/quanlv/calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.quanlv.calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/CalcButton.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CalcButton extends StatelessWidget {
+  final String text;
+  final Color color;
+  final textSize;
+  final Color textColor;
+  final Function callback;
+
+  const CalcButton(
+      {Key key,
+      this.text,
+      this.color,
+      this.textSize,
+      this.textColor,
+      this.callback})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: EdgeInsets.all(8.0),
+        child: SizedBox(
+          width: 65,
+          height: 65,
+          child: FlatButton(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(50.0)),
+            onPressed: () {
+              callback(text);
+            },
+            color: color != null ? color : null,
+            child: Text(
+              text,
+              style: GoogleFonts.rubik(
+                  textStyle: TextStyle(
+                      fontSize: textSize,
+                      color: textColor != null ? textColor : Colors.white)),
+            ),
+          ),
+        ));
+  }
+}
